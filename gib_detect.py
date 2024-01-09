fdcb6c5ec14fbe4b695df7d4cfacb440372db5d1
jzx10 <jzx1040798357@icloud.com>
2024-01-09 15:10:37 +0800

    update

--- gib_detect.py
+++ gib_detect.py
@@ -4,14 +4,15 @@ import pickle
 import gib_train
 
 model_data = pickle.load(open('gib_model.pki', 'rb'))
+model_mat = model_data['mat']
+threshold = model_data['thresh']
+print("Threshold: %s" % threshold)
 
 while True:
     l = input(">>> input:")
-    model_mat = model_data['mat']
-    threshold = model_data['thresh']
     res = gib_train.avg_transition_prob(l, model_mat)
     if res > threshold:
-        print("<<< [-] not Random")
+        print("<<< [-] not Random(%s)" % res)
     else:
-        print("<<< [+] is Random")
+        print("<<< [+] is Random(%s)" % res)
     print()
